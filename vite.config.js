371812e008c6fe1d0581be0a0f74d3283a19687e
Vitalii <zadavaka063@gmail.com>
2023-02-13 20:47:42 +0200

    add quasar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+
+    quasar(),
+  ],
+});
