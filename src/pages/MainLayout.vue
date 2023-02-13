b66c0994d381c1def68602ef132bf82dee0bb5d1
Vitalii <zadavaka063@gmail.com>
2023-02-14 00:10:43 +0200

    add style

--- src/pages/MainLayout.vue
+++ src/pages/MainLayout.vue
@@ -1,4 +1,61 @@
+<script setup>
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
+
+</script>
+
 <template>
-    Hello World
-    <router-view></router-view>
+    <q-layout view="hHh lpR fFf">
+
+      <q-header elevated class="text-white bg-blue-6" color="#f3dd31" >
+        <q-toolbar>
+            <q-btn dense flat round color="black" icon="menu" @click="toggleLeftDrawer" />
+
+            <div class="toolbar-title">Vue Test</div>
+
+            <q-space />
+
+            <div class="toolbar-user">Bayraktar</div>
+
+            <q-btn
+              flat
+              round
+              dense
+              color="black"
+              icon="exit_to_app"
+              @click="logout"
+              />
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+        <!-- drawer content -->
+      </q-drawer>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+
+    </q-layout>
+
 </template>
+
+<style scoped>
+
+.toolbar-title {
+  color: #000;
+  font-size: 20px;
+  margin-left: 10px;
+}
+
+.toolbar-user {
+  color: #000;
+  margin-right: 10px;
+}
+
+</style>
