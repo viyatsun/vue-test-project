4f353e257f7256f68a0ef1bc7441255c902c930c
Vitalii <zadavaka063@gmail.com>
2023-02-14 16:55:53 +0200

    init base url

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { createApp } from 'vue';
 import { Quasar } from 'quasar';
+import { createPinia } from 'pinia';
 import router from './router';
 
 import '@quasar/extras/material-icons/material-icons.css';
@@ -10,11 +11,23 @@ import 'quasar/dist/quasar.css';
 
 import App from './App.vue';
 
+import { TokenService } from './services/storage';
+import ApiService from './services/api';
+
+ApiService.init(import.meta.env.VITE_BASE_API_URL);
+
+if (TokenService.getToken()) {
+  ApiService.setHeader();
+}
+
 const myApp = createApp(App);
+const pinia = createPinia();
 
 myApp.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
 });
+
+myApp.use(pinia);
 
 myApp.use(router);
 
